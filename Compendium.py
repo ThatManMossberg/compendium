3758668f5acfbff9ce119ba80afede8288f3d08b
ThatManMossberg <115808039+ThatManMossberg@users.noreply.github.com>
2024-02-01 14:22:29 -0600

    Added source file
    4th attempt to fix root access issue

---
+++ Compendium.py
@@ -0,0 +1,67 @@
+# Associates part to number and number to part
+hardwareList = {"276-1028-001": "8-32 nut", "8-32 nut": "276-1028-001", "276-4991-001": "8-32*.375 in screw", "8-32 .375 screw": "276-4991-001", "276-5007-001": "8-32*.5 in screw", "276-4996-001": "8-32*1 in screw", "8-32*1 in screw": "276-4996-001"}
+electricalList = {"276-4810": "Brain", "brain": "276-4810", "276-4840": "Reg. Servo Motor", "motor": "276-4840", "276-4811": "battery", "battery": "276-4611", "276-4831": "Transmitter", "Transmitter": "276-4831"}
+constructionList = {"test": "test"}
+while True:
+    mode = input("Select Mode: ")
+#! Searches hardware
+    while True:
+        if mode == "hardware":
+            print("Enter Part name/code:")
+            x = str(input())
+            if x in hardwareList:
+                print(hardwareList[x])
+                print("")
+            elif x == "change mode":
+                break
+            else:
+                print("Not registered")
+                print("")
+#! Searches Electrical
+        elif mode == "electrical":
+            print("Enter Part name/code: ")
+            x = str(input())
+            if x in electricalList:
+                print(electricalList[x])
+                print("")
+#! Searches constuction
+        elif mode == "construction":
+            print("Enter Part name/code: ")
+            x = str(input())
+            if x in constructionList:
+                print(constructionList[x])
+                print("")
+#? Allows user to see what each mode does
+                #Working on it, I promise
+        
+        if mode == "mode?":
+            print("Enter hardware, electrical, construction, or change mode")
+            print("")
+            x = str(input())
+            if x == "hardware":
+                print("Hardware")
+                break
+            x = str(input())
+            if x == "electrical":
+                print("Electrical")
+                break
+            x = str(input())
+            if x == "construction":
+                print("construction")
+                break
+            x = str(input())
+            if x == "change mode":
+                break
+
+        
+
+    
+ 
+        elif x == "change mode":
+             break
+                
+        elif mode == "Stop":
+            quit()
+        else:
+            print("Not registered")
+            print("")
